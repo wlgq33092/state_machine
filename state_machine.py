22aa21416a2da0a61fc062c9965a0c62651ce2b8
wlgq33092 <wgfly1989@gmail.com>
2017-08-10 23:33:20 +0800

    first commit for state_machine

---
+++ state_machine.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+
+class State(object):
+	def __init__(self, name):
+		self.name = name
+		self.transition_dict = {}
+
+
+
+class StateMachine(object):
+	def __init__(self, start_state):
+		self.start_state = start_state
+		self.cur_state = start_state
+
+	def create_state(self):
+		pass
+
+class StateMachineBuilder(object):
+	def __init__(self):
+		pass
+
+class Transition(object):
+	def __init__(self):
+		pass
+
+
+class Pattern(object):
+	def __init__(self):
+		pass
+
+	def parse_pattern(self, input):
+		pass
