6fd817a6d781d045edce6abecb0b203e886076f2
wlgq33092 <wgfly1989@gmail.com>
2017-08-10 23:40:52 +0800

    tab to whitespaces

--- state_machine.py
+++ state_machine.py
@@ -1,35 +1,35 @@
 #! /usr/bin/env python
 
 class State(object):
-	def __init__(self, name):
-		self.name = name
-		self.transition_dict = {}
+    def __init__(self, name):
+        self.name = name
+        self.transition_dict = {}
 
-	def add_transition(self):
-		pass
+    def add_transition(self):
+        pass
 
 
 
 class StateMachine(object):
-	def __init__(self, start_state):
-		self.start_state = start_state
-		self.cur_state = start_state
+    def __init__(self, start_state):
+        self.start_state = start_state
+        self.cur_state = start_state
 
-	def create_state(self):
-		pass
+    def create_state(self):
+        pass
 
 class StateMachineBuilder(object):
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
 class Transition(object):
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
 
 class Pattern(object):
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def parse_pattern(self, input):
-		pass
+    def parse_pattern(self, input):
+        pass
